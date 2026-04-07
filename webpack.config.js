4b0d1d557e7479799bfcfdcb66d1b1241314c12e
SimonSchulz <zenkov241091@gmail.com>
2026-04-07 17:25:40 +0300

    add webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, argv) => ({
     entry: './src/index.js',
@@ -40,6 +41,11 @@ module.exports = (env, argv) => ({
     plugins: [
         new HtmlWebpackPlugin({
             template: './public/index.html'
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: 'public/robots.txt', to: 'robots.txt' }
+            ]
         })
     ]
 });
