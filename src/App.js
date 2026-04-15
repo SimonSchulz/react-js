0a55138782c345ebb626ead7c7aa0dc69be24e94
SimonSchulz <zenkov241091@gmail.com>
2026-04-15 12:13:23 +0300

    fix bug with check exp of token

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React, {Suspense} from 'react';
+import React, {Suspense, useEffect} from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/router';
+import {useGetMeQuery} from "./app/api";
+import {logout, setUser} from "./auth/authSlice";
+import {useDispatch, useSelector} from "react-redux";
 
 export default function App() {
+    const dispatch = useDispatch();
+    const token = useSelector((s) => s.auth.token);
+    const { data, error } = useGetMeQuery(undefined, {
+        skip: !token
+    });
+    useEffect(() => {
+        if (data) {
+            dispatch(setUser(data));
+        }
+        if (error) {
+            dispatch(logout());
+        }
+    }, [data, error, dispatch]);
     return (
         <Suspense fallback={<p>Loading...</p>}>
             <RouterProvider router={router} />
